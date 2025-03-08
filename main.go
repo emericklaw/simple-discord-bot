c7cb7066e39c0e883332b37a0a3514ebed8c77be
Matt Emerick-Law <matt@emericklaw.co.uk>
2025-03-08 04:02:20 +0000

    Bump version

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const applicationVersion string = "v0.7.7"
+const applicationVersion string = "v0.7.8"
 const buildDateTime string = ""
 
 var currentLogLevel string = "notice"
