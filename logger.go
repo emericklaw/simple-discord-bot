a9cec0f2b19c071079e63d4d889c63db350d8a97
Matt Emerick-Law <matt@emericklaw.co.uk>
2025-03-08 00:39:50 +0000

    Split code into separate files

---
+++ logger.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/spf13/viper"
+)
+
+func logger(logLevel string, format string, args ...interface{}) {
+
+	minLogLevel := currentLogLevel
+
+	if shouldLog(logLevel, minLogLevel) {
+
+		if dg == nil {
+			log.Println("⚠️ Discord session is nil")
+		}
+
+		// Format the string
+		message := fmt.Sprintf(format, args...)
+
+		logLevelEmoji := getLogLevelEmoji(logLevel)
+
+		// Log the error
+		log.Printf(logLevelEmoji+"%s", message)
+
+		if !viper.IsSet("_log_discord_channel_id") {
+			log.Printf(getLogLevelEmoji("emergency") + "No _log_discord_channel_id configured")
+		} else {
+			channelID := viper.GetString("_log_discord_channel_id")
+			sendEmbedMessageToDiscord(channelID, getLogLevelColor(logLevel), logLevelEmoji+strings.Title(logLevel), message)
+		}
+	}
+}
+
+func sendMessageToDiscord(channelID string, message string) {
+	if dg == nil {
+		log.Println("⚠️ Discord session is nil")
+	}
+
+	_, err := dg.ChannelMessageSend(channelID, message)
+	if err != nil {
+		logger("error", "Unable to send message to Discord: %s", err)
+	}
+}
+
+// Send Message
+func sendEmbedMessageToDiscord(channelID string, colour int, title string, message string) {
+	if dg == nil {
+		log.Println("⚠️ Discord session is nil")
+	}
+
+	// Create embed JSON structure
+	embed := &discordgo.MessageEmbed{
+		Title:       title,
+		Description: message,
+		Color:       colour,
+		Fields:      []*discordgo.MessageEmbedField{},
+	}
+
+	// Send the message to the specified channel
+	_, err := dg.ChannelMessageSendEmbed(channelID, embed)
+	if err != nil {
+		logger("error", "Error sending message: %s", err)
+	}
+}
+
+// Map of log levels to their color codes
+var logLevelColors = map[string]int{
+	"debug":     0x85929e, // Gray
+	"info":      0xaed6f1, // Light Blue
+	"notice":    0x3498db, // Blue
+	"warning":   0xeb984e, // Light Orange
+	"error":     0xFF0000, // Red
+	"critical":  0xd35400, // Dark Orange
+	"alert":     0x922b21, // Dark Red
+	"emergency": 0x6c3483, // Purple
+	"success":   0x2ecc71, // Green
+}
+
+// getLogLevelColor takes a log level as input and returns the corresponding color.
+func getLogLevelColor(level string) int {
+	// Check if the log level exists in the map, otherwise default to black (0x000000).
+	color, exists := logLevelColors[level]
+	if !exists {
+		logger("warning", "Unknown log level: %s. Defaulting to black.", level)
+		return 0x000000 // Black color for unknown log levels
+	}
+	return color
+}
+
+// Map of log levels to their relevant emojis
+var logLevelEmojis = map[string]string{
+	"debug":     "🐞",  // Bug Emoji (Debug)
+	"info":      "ℹ️", // Information Emoji (Info)
+	"notice":    "🔔",  // Bell Emoji (Notice)
+	"warning":   "⚠️", // Warning Emoji (Warning)
+	"error":     "❌",  // Error Emoji (Error)
+	"critical":  "🔥",  // Fire Emoji (Critical)
+	"alert":     "🚨",  // Police Car Light Emoji (Alert)
+	"emergency": "💀",  // Skull Emoji (Emergency)
+	"success":   "✅",  // Check Mark Emoji (Success)
+}
+
+// getLogLevelEmoji takes a log level as input and returns the corresponding emoji.
+func getLogLevelEmoji(level string) string {
+	// Check if the log level exists in the map, otherwise return a question mark emoji.
+	emoji, exists := logLevelEmojis[level]
+	if !exists {
+		logger("warning", "Unknown log level: %s. Defaulting to question mark emoji.", level)
+		return "❓ " // Default emoji for unknown log levels
+	}
+	return emoji + " "
+}
+
+// Define a map of log levels to their numeric values.
+var logLevels = map[string]int{
+	"debug":     1, // Debug is level 1
+	"info":      2, // Info is level 2
+	"notice":    3, // Notice is level 3
+	"warning":   4, // Warning is level 4
+	"error":     5, // Error is level 5
+	"critical":  6, // Critical is level 6
+	"alert":     7, // Alert is level 7
+	"emergency": 8, // Emergency is level 8
+	"success":   9, // Success is level 9
+}
+
+// Function to check if a log level should be triggered based on the minimum level.
+func shouldLog(currentLevel string, minLevel string) bool {
+	// Get the numeric value for both the current log level and the minimum level
+	currentLevelValue, currentExists := logLevels[currentLevel]
+	minLevelValue, minExists := logLevels[minLevel]
+
+	if !currentExists || !minExists {
+		log.Fatal("Unknown log level provided")
+	}
+
+	// If the current level is greater than or equal to the minimum level, log it.
+	return currentLevelValue >= minLevelValue
+}
